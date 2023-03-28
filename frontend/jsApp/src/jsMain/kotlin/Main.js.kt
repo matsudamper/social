20752c6def052c0d7a2d1ba0204faaab4f0c5c9d
matsudamper <let@matsudamper.net>
2023-03-29 03:01:50 +0900

    fix

--- frontend/jsApp/src/jsMain/kotlin/Main.js.kt
+++ frontend/jsApp/src/jsMain/kotlin/Main.js.kt
@@ -13,7 +13,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import kotlinx.browser.window
-import lib.InputOutsideClickFocusRelease
 import lib.NormalizeInputKeyCapture
 import navigation.NavController
 import navigation.Screen
@@ -32,74 +31,72 @@ fun main(args: Array<String>) {
             title = "social",
         ) {
             NormalizeInputKeyCapture {
-                InputOutsideClickFocusRelease {
-                    val density = LocalDensity.current
-                    var width by remember { mutableStateOf(0.dp) }
-                    var height by remember { mutableStateOf(0.dp) }
-                    fun updateSize() {
-                        width = with(density) {
-                            window.innerWidth.toDp() * density.density
-                        }
-                        height = with(density) {
-                            window.innerHeight.toDp() * density.density
-                        }
-                        println("updateSize: ($width, $height)")
+                val density = LocalDensity.current
+                var width by remember { mutableStateOf(0.dp) }
+                var height by remember { mutableStateOf(0.dp) }
+                fun updateSize() {
+                    width = with(density) {
+                        window.innerWidth.toDp() * density.density
                     }
-                    LaunchedEffect(density) {
-                        updateSize()
-                        window.addEventListener(
-                            type = "resize",
-                            callback = { updateSize() },
-                        )
+                    height = with(density) {
+                        window.innerHeight.toDp() * density.density
                     }
-                    CustomTheme {
-                        Box(
-                            modifier = Modifier
-                                .width(width)
-                                .height(height)
-                                .fillMaxSize(),
-                        ) {
-                            val navController = remember {
-                                NavController(
-                                    initial = Screen.Root,
-                                    directions = Screen.values().toList(),
+                    println("updateSize: ($width, $height)")
+                }
+                LaunchedEffect(density) {
+                    updateSize()
+                    window.addEventListener(
+                        type = "resize",
+                        callback = { updateSize() },
+                    )
+                }
+                CustomTheme {
+                    Box(
+                        modifier = Modifier
+                            .width(width)
+                            .height(height)
+                            .fillMaxSize(),
+                    ) {
+                        val navController = remember {
+                            NavController(
+                                initial = Screen.Root,
+                                directions = Screen.values().toList(),
+                            )
+                        }
+
+                        when (val current = navController.currentNavigation) {
+                            Screen.Root -> {
+                                Root(
+                                    text = current.title,
+                                    onClick = {
+                                        navController.navigate(Screen.Login)
+                                    },
                                 )
                             }
 
-                            when (val current = navController.currentNavigation) {
-                                Screen.Root -> {
-                                    Root(
-                                        text = current.title,
-                                        onClick = {
-                                            navController.navigate(Screen.Login)
-                                        },
+                            Screen.Login -> {
+                                val coroutineScope = rememberCoroutineScope()
+                                val viewModel = remember {
+                                    LoginScreenViewModel(
+                                        coroutineScope = coroutineScope,
                                     )
                                 }
+                                LoginScreen(
+                                    uiState = viewModel.uiStateFlow.collectAsState().value,
+                                )
+                            }
 
-                                Screen.Login -> {
-                                    val coroutineScope = rememberCoroutineScope()
-                                    val viewModel = remember {
-                                        LoginScreenViewModel(
-                                            coroutineScope = coroutineScope,
-                                        )
-                                    }
-                                    LoginScreen(
-                                        uiState = viewModel.uiStateFlow.collectAsState().value,
+                            Screen.Admin -> {
+                                val coroutineScope = rememberCoroutineScope()
+                                val viewModel = remember {
+                                    AdminScreenViewModel(
+                                        coroutineScope = coroutineScope,
+                                        adminSessionStorage = JsAdminSessionStorage(),
                                     )
                                 }
-
-                                Screen.Admin -> {
-                                    val coroutineScope = rememberCoroutineScope()
-                                    val viewModel = remember {
-                                        AdminScreenViewModel(
-                                            coroutineScope = coroutineScope,
-                                            adminSessionStorage = JsAdminSessionStorage(),
-                                        )
-                                    }
-                                    AdminRootScreen(
-                                        uiState = viewModel.uiStateFlow.collectAsState().value,
-                                    )
-                                }
+                                AdminRootScreen(
+                                    uiState = viewModel.uiStateFlow.collectAsState().value,
+                                )
                             }
                         }
                     }
--- frontend/jsApp/src/jsMain/kotlin/lib/NormalizeInputKeyCapture.kt
+++ frontend/jsApp/src/jsMain/kotlin/lib/NormalizeInputKeyCapture.kt
@@ -1,4 +1,7 @@
 package lib
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -7,37 +10,41 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.focusTarget
 import androidx.compose.ui.focus.onFocusChanged
 import kotlinx.browser.document
-import org.w3c.dom.events.Event
+import org.w3c.dom.events.KeyboardEvent
 
 @Composable
 public fun NormalizeInputKeyCapture(content: @Composable () -> Unit) {
     var hasFocus by remember {
         mutableStateOf(false)
     }
-    val callback: (Event) -> Unit = remember {
-        {
-            it.stopPropagation()
-        }
-    }
-    LaunchedEffect(hasFocus) {
+    LaunchedEffect(Unit) {
         val target = document.getElementById("ComposeTarget")!!
-        if (hasFocus) {
-            target.addEventListener(
-                type = "keydown",
-                callback = callback,
-            )
-        } else {
-            target.removeEventListener(
-                type = "keydown",
-                callback = callback,
-            )
-        }
+        target.addEventListener(
+            type = "keydown",
+            callback = { event ->
+                event as KeyboardEvent
+
+                if (hasFocus) {
+                    event.stopImmediatePropagation()
+                }
+            },
+        )
     }
 
+    val focusRequester = remember { FocusRequester() }
     Box(
         modifier = Modifier
+            .focusTarget()
+            .focusRequester(focusRequester)
+            .clickable(
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() },
+            ) { focusRequester.freeFocus() }
             .onFocusChanged {
                 hasFocus = it.hasFocus
             },
